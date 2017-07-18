31fb7d7eb114fe3107e33be8ab219bae1f449108
Johanna Staedel <bluecherryme@gmail.com>
2017-07-18 10:28:45 -0600

    finished overlay

--- src/Components/Cards/SingleCard/SingleCard.js
+++ src/Components/Cards/SingleCard/SingleCard.js
@@ -5,14 +5,26 @@ import './SingleCard.css';
 export default class SingleCard extends Component{
 
     render(){
-        var {id,title,usedIngredientCount} = this.props.recipe;
+        var {id,title,usedIngredientCount,missedIngredientCount,likes,readyInMinutes} = this.props.recipe;
         return(
             <div>
                 <div className='col-md-4 recipe-card' key={id} 
                     style={{"backgroundImage":`url(${this.props.image})`}}>
                     
                     <div className="overlay">
-                        
+                        <p>{title}</p>
+                        {
+                            usedIngredientCount
+                            ?
+                            <div className="overlayByIngredients">
+                                <p>used Ingredient Count: {usedIngredientCount}</p>
+                                <p>missed Ingredient Count: {missedIngredientCount}</p> 
+                                <p>likes:{likes}</p>
+                            </div>
+                            :
+                            <p>prep-time: {readyInMinutes}</p>
+                        }
+                                                                    
                     </div>                                   
                 </div>
             </div>
