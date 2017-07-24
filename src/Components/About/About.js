7b92343b7c345e3758b57487ae99880e8f50e211
Johanna Staedel <bluecherryme@gmail.com>
2017-07-24 10:08:20 -0600

    changed readme

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import Footer from './../Footer/footer';
 
 export default class About extends Component{
     render(){
         return(
             <div className="About">
                 <h1>About</h1>
+                <h2>Technologies used to build this page:</h2>
+                <div className="icons">
+                    
+                </div>
+                <h2>Have a look at the code that runs this page 
+                    <a href='https://github.com/bluecherryme/RecipeApp'>here</a>
+                </h2>
+                <Footer/>
             </div>
         );
     }
